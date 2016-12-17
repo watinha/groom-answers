17c62eac8ac995d86e252039ebd922c8440f0f26
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-12-17 15:07:57 -0200

    improving the layout of the messages

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 var textarea = document.querySelector('textarea'),
     messages = document.querySelector('ul'),
+    timer = 0,
     g = GroomBot(
         ['', 'zzz.', 'uhm...', 'uhm ', 'Então...', 'oi. ', 'oi ', 'OI ', '...', 'Hum.', 'Hmmmm...', 'Errrm...', 'mmmm...', ''],
         ['OK', 'ok', 'Parece legal', 'legal', 'achei legal', 'parece legal', 'whatever', 'você que sabe', 'pode ser.',
@@ -12,6 +13,8 @@ document.querySelector('input[type=submit]').addEventListener('click', function 
         groom_msg = document.createElement('li'),
         bride_avatar = document.createElement('span'),
         groom_avatar = document.createElement('span');
+    if (textarea.value.length === 0)
+        return ;
     bride_msg.className = 'bride';
     bride_avatar.className = 'avatar';
     bride_msg.appendChild(bride_avatar);
@@ -19,11 +22,16 @@ document.querySelector('input[type=submit]').addEventListener('click', function 
     textarea.value = '';
     messages.appendChild(bride_msg);
 
-    groom_msg.className = 'groom';
+    groom_msg.className = 'groom hidden';
     groom_avatar.className = 'avatar';
     groom_msg.appendChild(groom_avatar);
     groom_msg.innerHTML += g.answer();
     messages.appendChild(groom_msg);
+
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+        groom_msg.className = 'groom';
+    }, 3000);
 
     ev.preventDefault();
     ev.stopPropagation();
